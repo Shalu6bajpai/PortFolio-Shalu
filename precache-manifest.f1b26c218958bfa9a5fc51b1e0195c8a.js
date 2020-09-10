4b5924d25ae352290e7ebb74a860014aadfb408f
Shalu6bajpai <bajpaishalu6@gmail.com>
2020-09-10 08:33:40 -0700

    Updates

---
+++ precache-manifest.f1b26c218958bfa9a5fc51b1e0195c8a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc2f02f88bc371fc8f67e18eaef88a6",
+    "url": "/PortFolio-Shalu/index.html"
+  },
+  {
+    "revision": "04e353cd87adc0ddb4bd",
+    "url": "/PortFolio-Shalu/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5ee04b1d53404d1112a7",
+    "url": "/PortFolio-Shalu/static/css/main.5f50cc93.chunk.css"
+  },
+  {
+    "revision": "04e353cd87adc0ddb4bd",
+    "url": "/PortFolio-Shalu/static/js/2.236c19ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortFolio-Shalu/static/js/2.236c19ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee04b1d53404d1112a7",
+    "url": "/PortFolio-Shalu/static/js/main.813f01a7.chunk.js"
+  },
+  {
+    "revision": "8990a26d3a7d547994d8",
+    "url": "/PortFolio-Shalu/static/js/runtime-main.feb284fc.js"
+  },
+  {
+    "revision": "f06dfb5045dd20ad17ebe51558a006fe",
+    "url": "/PortFolio-Shalu/static/media/codepen.f06dfb50.svg"
+  },
+  {
+    "revision": "f623a97e06af2a64f6e74d5b2d6afb27",
+    "url": "/PortFolio-Shalu/static/media/facebook.f623a97e.svg"
+  },
+  {
+    "revision": "603e73d9fb5bc5be5ee0362a91e7efa6",
+    "url": "/PortFolio-Shalu/static/media/github.603e73d9.svg"
+  },
+  {
+    "revision": "418d4806ab3579d7023d86662f7d9665",
+    "url": "/PortFolio-Shalu/static/media/linkedin.418d4806.svg"
+  },
+  {
+    "revision": "5b814a272e00e4f77e4c94637ff0274d",
+    "url": "/PortFolio-Shalu/static/media/mail.5b814a27.svg"
+  },
+  {
+    "revision": "e5de11d64c765baa84e0db92269aab24",
+    "url": "/PortFolio-Shalu/static/media/medium.e5de11d6.svg"
+  },
+  {
+    "revision": "de1356b5b9bf84bb1e36604e0ab284d8",
+    "url": "/PortFolio-Shalu/static/media/quora.de1356b5.svg"
+  },
+  {
+    "revision": "fce4a5c9f11eb982661a348d87fb783e",
+    "url": "/PortFolio-Shalu/static/media/twitter.fce4a5c9.svg"
+  }
+]);
